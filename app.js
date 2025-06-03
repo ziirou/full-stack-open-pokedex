a7d5280924623b7a8fd4247c48139bc39145df5f
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-06-03 14:55:39 +0300

    Exercise 11.12 - part4
    fix the deployment

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
