de5b749ac2a16118a636deb10190bb7d5c5e5753
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-06-03 10:37:58 +0300

    Exercise 11.9 - Add beforeEach

--- e2e-tests/Pokedex.spec.js
+++ e2e-tests/Pokedex.spec.js
@@ -1,8 +1,11 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
